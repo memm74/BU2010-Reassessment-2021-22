11ae4ee19246a7c6278ab0ffe71d2fdd4897338d
Matthias <memm74@users.noreply.github.com>
2022-06-28 15:55:44 +0100

    added content

---
+++ reassessment.R
@@ -0,0 +1,23 @@
+# Set the correct working directory for your machine, i.e. the directory you cloned this repo to
+setwd()
+
+# Load the packages you need
+library()
+
+# Read (import) the html file with The Pen Addict Podcast information. It is in this repo. 
+
+
+# Scrape the episode number, title, date and length into four vectors. (Hint: use SelectorGadget to help you, like we have done in class)
+
+
+# Combine the four vectors into one data frame.
+
+
+#For the next task please load the following data frame (called data)
+load(file="data.Rda")
+
+#Create a Scatterplot with the date (Date) on the x-axis and the length of the episode (Length) on the y-axis. 
+#Give the plot a title and label the axes. 
+#Remember to load any packages you might need
+
+
